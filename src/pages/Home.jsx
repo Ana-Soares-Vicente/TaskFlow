05b3c504ac974e53636b6c1b515bbf8c18c14dfc
Ana-Soares-Vicente <a.vicente@catolicasc.edu.br>
2026-05-25 11:35:43 -0300

    style:"adicionei uma imagem para teste"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,11 @@ const Home = () => {
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Bem-vindo ao TaskFlow</h1>
       <p>Use o menu para navegar para projetos ou fazer login.</p>
+         <img
+        src="https://servircomrequinte.francobachot.com.br/wp-content/uploads/2025/02/CAFETERIAS-FB.jpg"
+        alt="Produtos"
+        className="w-full h-auto mt-4 rounded-lg shadow-md object-cover w-64 h-64 flex items-center justify-center"
+      />
     </div>
   );
 };
